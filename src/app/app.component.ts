f097e9398fca56a49f21a44db7ebf7309ac3ced1
camila <cami.villarreal1@gmail.com>
2025-06-12 13:34:59 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// Es posible que tengas 'RouterOutlet' aquí, déjalo si está.
+// import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './Vistas/login/login.component'; // <-- 1. IMPORTAS EL COMPONENTE
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [LoginComponent], // <-- 2. LO AÑADES A LA LISTA DE IMPORTS
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'pedidos-ya';
