ca4c81a84b385b19da874327ba065f3492578602
Agustin Liendo Ortiz <152560063+AgusLiendo@users.noreply.github.com>
2025-06-12 15:04:16 -0300

    Add registration functionality to register component; integrate API call and navigation

--- src/app/Vistas/register/register.component.ts
+++ src/app/Vistas/register/register.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router'; // Importa RouterLink para la navegación
+import { RouterLink, Router } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {config} from '../../config/env';
+import axios from 'axios'; // Importa RouterLink para la navegación
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, RouterLink], // Añade RouterLink a los imports
+  imports: [CommonModule, RouterLink, FormsModule], // Añade RouterLink a los imports
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
@@ -13,7 +16,7 @@ export class RegisterComponent {
   // Variable para controlar si la contraseña se muestra o no
   showPassword = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   // Función para alternar la visibilidad de la contraseña
   togglePasswordVisibility(): void {
@@ -23,6 +26,19 @@ export class RegisterComponent {
   // Función que se ejecuta al enviar el formulario
   crearCuenta(): void {
     // Aquí irá la lógica para registrar al usuario
-    console.log("Intentando crear cuenta...");
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const password = (document.getElementById('password') as HTMLInputElement).value;
+
+    axios.post(config.urls.postRegister, {
+      email,
+      password
+    })
+      .then(res => {
+        console.log('Registro exitoso:', res.data);
+        this.router.navigate(['/login']); // Redirige al usuario a la página de inicio de sesión
+      })
+      .catch(err => {
+        console.error('Error al registrar:', err);
+      })
   }
 }
